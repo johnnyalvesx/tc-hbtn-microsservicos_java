f94de20b17a8f9e0ff9d469c6d1add475229e701
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-31 18:15:13 -0300

    Primeiro microsserviço (endpoint) com Spring Boot

---
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -0,0 +1,29 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+
+	return "BEM VINDO A AULA DE MICROSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login")
+    public String login(String user, String senha) {
+
+	user = "Johnny";
+	senha = "87654321";
+
+	if (user.isEmpty() || senha.isEmpty()) {
+	    return "USUÁRIO E SENHA NÃO INFORMADOS";
+	} else if (user.length() > 15 || senha.length() > 15) {
+	    return "USUÁRIO E SENHA INVÁLIDOS";
+	} else {
+	    return "LOGIN EFETUADO COM SUCESSO !!!";
+	}
+    }
+}
